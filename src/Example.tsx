414d1a7380462b6d99f5826ec4fdc68eaefe87b1
Valerii Bubenshchikov <augustbright@yandex.com>
2023-11-14 22:52:28 +0400

    Initial commit

---
+++ src/Example.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+    DataPickerRow, FlexCell, PickerInput, PickerItem,
+} from '@epam/promo';
+import {
+    DataQueryFilter, DataRowProps, DataSourceState, useLazyDataSource, useUuiContext,
+} from '@epam/uui-core';
+import { Location } from '@epam/uui-docs';
+
+export default function LazyTreePicker() {
+    const svc = useUuiContext();
+    const [value, onValueChange] = useState<string[]>([]);
+
+    const dataSource = useLazyDataSource<Location, string, DataQueryFilter<Location>>(
+        {
+            api: (request, ctx) => {
+                const { search } = request;
+                const filter = { parentId: ctx?.parentId };
+                return svc.api.demo.locations({ ...request, search, filter });
+            },
+            cascadeSelection: true,
+            flattenSearchResults: true,
+            getId: (i) => i.id,
+            getParentId: (i) => i.parentId,
+            getChildCount: (l) => l.childCount,
+        },
+        [],
+    );
+
+    const getSubtitle = ({ path }: DataRowProps<Location, string>, { search }: DataSourceState) => {
+        if (!search) return;
+
+        return path
+            .map(({ value: v }) => v?.name)
+            .filter(Boolean)
+            .join(' / ');
+    };
+
+    return (
+        <FlexCell width={ 300 }>
+            <PickerInput
+                dataSource={ dataSource }
+                value={ value }
+                onValueChange={ onValueChange }
+                entityName="location"
+                selectionMode="multi"
+                valueType="id"
+                cascadeSelection="explicit"
+                renderRow={ (props: DataRowProps<Location, string>, dataSourceState) => (
+                    <DataPickerRow
+                        { ...props }
+                        key={ props.rowKey }
+                        padding="12"
+                        renderItem={ (item, rowProps) => (
+                            <PickerItem 
+                                { ...props }
+                                { ...rowProps } 
+                                title={ item.name }
+                                dataSourceState={ dataSourceState }
+                                subtitle={ getSubtitle(props, dataSourceState) } 
+                            />
+                        ) }
+                    />
+                ) }
+            />
+        </FlexCell>
+    );
+}
---
+++ src/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { ErrorHandler, FlexRow, skinContext } from '@epam/promo';
+import {
+    HistoryAdaptedRouter,
+    IProcessRequest,
+    useUuiServices,
+    UuiContext,
+} from '@epam/uui-core';
+import { Modals, Snackbar } from '@epam/uui-components';
+import '@epam/uui-components/styles.css';
+// eslint-disable-next-line import/no-unresolved
+import '@epam/uui/styles.css';
+import '@epam/promo/styles.css';
+import '@epam/loveship/styles.css';
+import Example from './Example';
+import { svc, getApi } from './api';
+
+type TApi = ReturnType<typeof getApi>;
+
+const rootElement = document.getElementById('root');
+const origin = process.env.REACT_APP_PUBLIC_URL;
+
+const history = createBrowserHistory();
+const router = new HistoryAdaptedRouter(history);
+
+function apiDefinition(processRequest: IProcessRequest) {
+    return getApi({ processRequest, fetchOptions: { credentials: undefined }, origin });
+}
+
+function UuiEnhancedApp() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const { services } = useUuiServices<TApi, never>({ apiDefinition, router, skinContext });
+
+    useEffect(() => {
+        Object.assign(svc, services);
+        setIsLoaded(true);
+    }, [services]);
+
+    if (isLoaded) {
+        return (
+            <UuiContext.Provider value={ services }>
+                <ErrorHandler>
+                    <FlexRow vPadding="48" padding="24" borderBottom alignItems="top" spacing="12">
+                        <Example />
+                    </FlexRow>
+                    <Snackbar />
+                    <Modals />
+                </ErrorHandler>
+            </UuiContext.Provider>
+        );
+    }
+    return null;
+}
+
+render(
+    <UuiEnhancedApp />,
+    rootElement,
+);
